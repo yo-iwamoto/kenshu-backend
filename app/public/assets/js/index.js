4cc44a2a0e9b446b9b0e519bea90a4b6ddad76ba
you-5805 <you.iwamoto@prtimes.co.jp>
2022-05-18 10:26:05 +0900

    fix: js

--- app/public/assets/js/index.js
+++ app/public/assets/js/index.js
@@ -1,22 +1,24 @@
 /**
  * @description preview chosen image
  * @requires input#image, div#preview-container
+ * TODO: 一度プレビューを表示した後画像を変更すると壊れる
  */
 const previewImageWhenImageChosen = () => {
   const $imageInput = document.getElementById('image');
   const $previewContainer = document.getElementById('preview-container');
+  const $preview = document.getElementById('preview');
 
   if ($imageInput === null || $previewContainer === null) {
     throw new Error('invalid DOM');
   }
 
   $imageInput.addEventListener('change', (e) => {
-    console.log(e.target);
     if (e.target instanceof HTMLInputElement) {
       const url = window.URL.createObjectURL(e.target.files[0]);
 
       const $image = document.createElement('img');
       $image.src = url;
+      $image.id = 'preview';
       $image.alt = 'プロフィール画像のプレビュー';
       $image.width = 200;
 
