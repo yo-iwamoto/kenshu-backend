3f1b494d6fb437fa4736d7970ad3021b2818da42
you-5805 <you.iwamoto@prtimes.co.jp>
2022-06-03 15:19:21 +0900

    remove unused code

--- app/services/concerns/ServiceWithId.php
+++ app/services/concerns/ServiceWithId.php
@@ -1,19 +1,14 @@
 <?php
 namespace App\services\concerns;
 
-use App\lib\PDOFactory;
 use App\lib\Request;
-
-use PDO;
 
 abstract class ServiceWithId extends Service
 {
-    protected PDO $pdo;
-    
     public function __construct(
         protected Request $request,
         protected string $id,
     ) {
-        $this->pdo = PDOFactory::create();
+        parent::__construct($request);
     }
 }
