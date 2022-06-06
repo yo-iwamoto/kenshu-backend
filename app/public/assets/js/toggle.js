eefd7bf87e97cbe79d25b86446c126235b01d210
you-5805 <you.iwamoto@prtimes.co.jp>
2022-06-06 16:56:39 +0900

    fix: js

--- app/public/assets/js/toggle.js
+++ app/public/assets/js/toggle.js
@@ -1,20 +1,24 @@
-const $toggleButton = document.getElementById('js-toggle-button');
-const $toggleBody = document.getElementById('js-toggle-body');
+const toggle = () => {
+  const $toggleButton = document.getElementById('js-toggle-button');
+  const $toggleBody = document.getElementById('js-toggle-body');
 
-if ($toggleButton === null || $toggleBody === null) {
-  throw new Error();
-}
-
-let isOpen = true;
-
-$toggleButton.addEventListener('click', () => {
-  if (isOpen) {
-    $toggleBody.classList.add('h-0');
-    $toggleButton.classList.add('rotate-180');
-    isOpen = false;
-  } else {
-    $toggleBody.classList.remove('h-0');
-    $toggleButton.classList.remove('rotate-180');
-    isOpen = true;
+  if ($toggleButton === null || $toggleBody === null) {
+    return;
   }
-});
+
+  let isOpen = true;
+
+  $toggleButton.addEventListener('click', () => {
+    if (isOpen) {
+      $toggleBody.classList.add('h-0');
+      $toggleButton.classList.add('rotate-180');
+      isOpen = false;
+    } else {
+      $toggleBody.classList.remove('h-0');
+      $toggleButton.classList.remove('rotate-180');
+      isOpen = true;
+    }
+  });
+};
+
+toggle();
